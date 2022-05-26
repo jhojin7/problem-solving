ee39063dd79adadf0e24ab12e9df2f2c1b5c78d1
Hojin Jang <jhojin7@gmail.com>
2022-05-26 12:59:09 +0900

    number-of-1-bits

---
+++ leetcode/number-of-1-bits.py
@@ -0,0 +1,20 @@
+"""
+https://leetcode.com/submissions/detail/707357173/
+https://en.wikipedia.org/wiki/Hamming_weight
+https://docs.python.org/3.10/library/stdtypes.html#int.bit_count
+int.bit_count() since python3.10
+"""
+class Solution:
+    def hammingWeight(self,n:int):
+        # return str(n).count('1')
+        # return sum([int(x) for x in list(str(n))])
+        return (bin(n).count('1'))
+
+# n = int("00000000000000000000000000001011")#3
+# n = int("00000000000000000000000010000000")#1
+# n = int("11111111111111111111111111111101")#31
+# print(Solution().hammingWeight(n))
+# 11
+# 128
+# 4294967293
+print(Solution().hammingWeight(11))
