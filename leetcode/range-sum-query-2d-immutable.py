e43cba9fade3f0c864efced5c376ff8d3e0a984b
Hojin Jang <jhojin7@gmail.com>
2022-06-03 16:59:57 +0900

    range-sum-query-2d-immutable

---
+++ leetcode/range-sum-query-2d-immutable.py
@@ -0,0 +1,36 @@
+"""
+index out of range
+https://leetcode.com/submissions/detail/713395396/
+https://leetcode.com/problems/range-sum-query-2d-immutable/discuss/2104545/O(m*n)-to-O(n)-to-O(1)-Search-Time-oror-C++
+"""
+from __solver import *
+class NumMatrix:
+    def __init__(self, matrix: List[List[int]]):
+        self.mat1 = matrix
+        self.mat2 = [[0 for _ in range(len(matrix)+1)] for _ in range(len(matrix[0])+1)]
+        
+        for i in range(1,len(matrix)+1):
+            for j in range(1,len(matrix[0])+1):
+                self.mat2[i][j] = \
+                self.mat1[i-1][j-1]\
+                + self.mat2[i-1][j]\
+                + self.mat2[i][j-1]\
+                - self.mat2[i-1][j-1]
+        # print(self.mat1,self.mat2)
+
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        # print(self.mat2)
+        return self.mat2[row2+1][col2+1]\
+            - self.mat2[row1][col2+1]\
+            - self.mat2[row2+1][col1]\
+            + self.mat2[row1][col1]
+
+# Your NumMatrix object will be instantiated and called as such:
+# ["NumMatrix","sumRegion","sumRegion","sumRegion"]
+matrix = [[3,0,1,4,2],[5,6,3,2,1],[1,2,0,1,5],[4,1,0,1,7],[1,0,3,0,5]]
+row1,col1,row2,col2 = [2,1,4,3]
+# [1,1,2,2]
+# [1,2,2,4]
+obj = NumMatrix(matrix)
+param_1 = obj.sumRegion(row1,col1,row2,col2)
+print(param_1)
