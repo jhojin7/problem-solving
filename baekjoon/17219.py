1c277cbbf4bdbd5984d426cd76d1215ad9d687f6
Hojin Jang <jhojin7@gmail.com>
2022-06-11 18:39:18 +0900

    17219 비밀번호찾기

---
+++ baekjoon/17219.py
@@ -0,0 +1,42 @@
+"""
+problem: https://www.acmicpc.net/problem/17219
+ac: https://www.acmicpc.net/source/44427342
+"""
+import sys, io
+tc = """
+16 4
+noj.am IU
+acmicpc.net UAENA
+startlink.io THEKINGOD
+google.com ZEZE
+nate.com VOICEMAIL
+naver.com REDQUEEN
+daum.net MODERNTIMES
+utube.com BLACKOUT
+zum.com LASTFANTASY
+dreamwiz.com RAINDROP
+hanyang.ac.kr SOMEDAY
+dhlottery.co.kr BOO
+duksoo.hs.kr HAVANA
+hanyang-u.ms.kr OBLIVIATE
+yd.es.kr LOVEATTACK
+mcc.hanyang.ac.kr ADREAMER
+startlink.io
+acmicpc.net
+noj.am
+mcc.hanyang.ac.kr
+"""
+sys.stdin = io.StringIO(tc.strip())
+
+import collections
+N, M = map(int,input().split()) 
+# passwords[site] = pw
+passwords = collections.defaultdict()
+# input
+for _ in range(N):
+    site, pw = map(str,input().split()) 
+    passwords[site] = pw
+# search
+for _ in range(M):
+    _site = str(input())
+    print(passwords[_site])
