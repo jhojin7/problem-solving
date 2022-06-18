e0e4bb4d8048099d0bfff09894979c8c82f838e0
Hojin Jang <jhojin7@gmail.com>
2022-06-18 23:09:05 +0900

    1927 최소힙

--- baekjoon/1927.py
+++ baekjoon/1927.py
@@ -1,66 +1,31 @@
 """
-problem:https://www.acmicpc.net/problem/1927
-ac:https://www.acmicpc.net/source/44487244
+id: 1927
+title:최소힙
+ac:https://www.acmicpc.net/source/44700041 
 """
-tc = """
-6
-1
+import sys
+from io import StringIO
+sys.stdin = StringIO("""
+9
+0
+12345678
 1
 2
 0
 0
 0
-"""
-import sys
-from __boj import BOJ
-BOJ(tc)
-class minheap:
-    def __init__(self) -> None:
-        self.heap = []
+0
+32
+""".strip())
 
-    def push(self, x):
-        added = False
-        for i in range(len(self.heap)):
-            if self.heap[i] > x:
-                self.heap.insert(i,x)
-                added = True
-                break
-        if added == False:
-            self.heap.append(x)
-
-    def pop(self):
-        if len(self.heap) == 0:
-            print(0)
-        else:
-            print(self.heap[0])
-            self.heap.pop(0)
-
-# N = int(input())
-# heap = minheap()
-# for _ in range(N):
-#     x = int(input()) 
-#     if x == 0:
-#         heap.pop()
-#     else:
-#         heap.push(x)
-
-import heapq, sys
-N = int(sys.stdin.readline())
-h = []
-result = []
+import sys, heapq
+input = sys.stdin.readline
+N = int(input())
+minheap = []
 for _ in range(N):
-    # print(heap)
-    x = int(sys.stdin.readline()) 
-    # pop
+    x = int(input())
     if x == 0:
-        if len(h) == 0:
-            result.append(0)
-            # print(0)
-        else:
-            result.append(heapq.heappop(h))
-            # print(heapq.heappop(heap))
-    # push
+        try: print(heapq.heappop(minheap))
+        except IndexError: print(0)
     else:
-        heapq.heappush(h,x)
-for i in range(len(result)):
-    print(result[i])
+        heapq.heappush(minheap, x)
