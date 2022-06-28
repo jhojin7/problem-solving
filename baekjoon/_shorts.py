a03502bdb460a69c150a169f16edcda561252630
Hojin Jang <jhojin7@gmail.com>
2022-06-29 00:37:57 +0900

    25305 커트라인

--- baekjoon/_shorts.py
+++ baekjoon/_shorts.py
@@ -1,14 +1,23 @@
 """ Shorts """
 
-""" 2407 조합"""
-def factorial(x):
-    if x == 1 or x == 0: return 1
-    elif x == 2: return 2
-    return factorial(x-1) * x
-# n,m = map(int, input().split())
-n,m = 100, 6
-dp = [1,1,2] 
-for i in range(3,100+1):
-    dp.append(dp[i-1]*i)
-    # dp[i] = dp[i-1] * i
-print((dp[n]//dp[n-m])//dp[m])
+def q2407():
+    """ 2407 조합"""
+    def factorial(x):
+        if x == 1 or x == 0: return 1
+        elif x == 2: return 2
+        return factorial(x-1) * x
+    # n,m = map(int, input().split())
+    n,m = 100, 6
+    dp = [1,1,2] 
+    for i in range(3,100+1):
+        dp.append(dp[i-1]*i)
+        # dp[i] = dp[i-1] * i
+    print((dp[n]//dp[n-m])//dp[m])
+
+def q25305():
+    # https://www.acmicpc.net/source/45174791
+    N,k = map(int,input().split())
+    scores = sorted(list(map(int,input().split())),reverse=True)
+    print(N,k,scores)
+    print(scores[k-1])
+    pass
