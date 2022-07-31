a11f1728802519c863252fd625daac194b258536
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-07-31 20:57:24 +0900

    [Silver I] Title: 1로 만들기 2, Time: 152 ms, Memory: 114280 KB -BaekjoonHub

--- 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
+++ 백준/Silver/12852. 1로 만들기 2/1로 만들기 2.py
@@ -6,7 +6,6 @@ q = collections.deque([(N,0)])
 visited = set()
 while q:
     x,cnt = q.popleft()
-    # print(x,prev)
     if x in visited: continue 
     visited.add(x)
     if x==1:
@@ -18,12 +17,10 @@ while q:
             prev[x//3] = x
         q.append((x//3,cnt+1))
     if x%2==0 and x//2 not in visited:
-        # if x//2 in visited: continue
         if x//2 not in prev.keys():
             prev[x//2] = x
         q.append((x//2,cnt+1))
     if 0<x-1 and x-1 not in visited:
-        # if x-1 in visited: continue
         if x-1 not in prev.keys():
             prev[x-1] = x
         q.append((x-1,cnt+1))
