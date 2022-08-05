77663c10bc89df9ea841a63ac22102c738b74758
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-08-06 00:27:33 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    string str;
+    cin >> str;
+    sort(str.rbegin(),str.rend());
+    cout << str;
+    return 0;
+}
