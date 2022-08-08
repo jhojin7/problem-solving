c4f31d0e9eea44168fedfbbc54a79af8cf40c553
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-08-09 03:33:39 +0900

    [Bronze III] Title: 택시 기하학, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3053. 택시 기하학/택시 기하학.cc
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    ios::sync_with_stdio(0);
+    double r;
+    cin >> r;
+    printf("%.6f\n",M_PI*r*r);
+    printf("%.6f\n",2*r*r);
+    return 0;
+}
