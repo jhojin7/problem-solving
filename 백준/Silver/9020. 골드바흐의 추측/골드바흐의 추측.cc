9f07a1791ad5decd29359f9977412569bf984b3b
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-08-10 16:20:07 +0900

    [Silver II] Title: 골드바흐의 추측, Time: 296 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.cc
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<bool> primes (10003,true);
+int main(){
+    ios::sync_with_stdio(0);
+    // calc primes
+    primes[1] = false;
+    for (int i=2;i*i<=10000;i++){
+        if (!primes[i]) continue;
+        for (int j=i*i; j<=10000; j+=i)
+            primes[j] = false;
+    }
+    // testcase
+    int t;
+    cin >> t;
+    while (t--){
+        int mini=0,minj=0;
+        int mindiff = 99999999;
+        int n;
+        cin >> n;
+        for (int i=2;i*i<=n*n;i++){
+            int j = n-i;
+            if (0<j<n and primes[i] and primes[j]){
+                // cout << i << ' ' << j << '\n';
+                int diff = abs(j-i);
+                if (diff < mindiff){
+                    mindiff = diff;
+                    mini = i;
+                    minj = j;
+                }
+            }
+        }
+        cout << mini << ' ' << minj << '\n';
+    }
+    return 0;
+}
