db147550e9a3a2105737b707c9b7923489e63aa3
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-09-15 12:32:28 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 176 ms, Memory: 116320 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -0,0 +1,10 @@
+import heapq
+n,m=map(int,input().split())
+a=list(map(int,input().split()))
+heapq.heapify(a)
+for _ in range(m):
+    x = heapq.heappop(a)
+    y = heapq.heappop(a)
+    heapq.heappush(a,x+y)
+    heapq.heappush(a,x+y)
+print(sum(a))
