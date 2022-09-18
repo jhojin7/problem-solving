8adf6650465c3261a412779ee2723a68e7d3cf1b
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-09-18 12:29:54 +0900

    [Silver V] Title: UCPC는 무엇의 약자일까?, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.py
+++ 백준/Silver/15904. UCPC는 무엇의 약자일까？/UCPC는 무엇의 약자일까？.py
@@ -1,31 +1,8 @@
 s=input()
-chars=[]
-uu=[]
+t="UCPC"
+j=0
 for i in range(len(s)):
-    if s[i]=='U':
-        chars.append((i,s[i]))
-        uu.append(i)
-    elif s[i]=='C': chars.append((i,s[i]))
-    elif s[i]=='P': chars.append((i,s[i]))
-# print(chars)
-
-if len(chars)<4:
-    print("I hate UCPC")
-    exit()
-
-# cnt=0
-for i in uu:
-    ans=[s[i]]
-    for j,ch in chars:
-        if j<=i: continue
-        # print(j,ch)
-        if ans[-1]=='U' and ch=='C':
-            ans.append(ch)
-        elif ans[-1]=='C' and ch=='P':
-            ans.append(ch)
-        elif ans[-1]=='P' and ch=='C':
-            ans.append(ch)
-            print("I love UCPC")
-            exit()
-
-print("I hate UCPC")
+    if s[i]==t[j]: j+=1
+    if j==4: break
+if j==4: print("I love UCPC")
+else: print("I hate UCPC")
