e1361d5c9019f63106c18a9a654b571183a46967
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-11-18 10:25:03 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,6 @@
+select ins.animal_id, ins.name -- , ins.datetime, outs.datetime
+from animal_ins as ins
+join animal_outs as outs 
+on ins.animal_id = outs.animal_id
+where ins.datetime >= outs.datetime
+order by ins.datetime asc
