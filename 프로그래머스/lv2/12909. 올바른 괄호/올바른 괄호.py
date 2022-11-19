dfcc4470b87f67cfd4b472db4ee50f7327a78652
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-11-20 03:47:51 +0900

    [level 2] Title: 올바른 괄호, Time: 11.34 ms, Memory: 11.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -2,9 +2,7 @@ def solution(s):
     s = list(s.strip())
     stack = [s.pop(0)]
     for c in s:
-        if not stack:
-            stack.append(c)
-        elif stack[-1]=='(' and c==')':
+        if stack and stack[-1]=='(' and c==')':
             stack.pop()
         else:
             stack.append(c)
