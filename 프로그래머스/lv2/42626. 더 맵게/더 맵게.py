ff1211dcf29af22254c0d216d8ca95c028a8f1e2
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2022-11-24 15:00:28 +0900

    [level 2] Title: 더 맵게, Time: 2099.91 ms, Memory: 51.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,15 @@
+import heapq
+def solution(arr, K):
+    heapq.heapify(arr)
+    i = 0
+    while len(arr)>=2 and arr[0]<K:
+        i+=1
+        a = heapq.heappop(arr)
+        b = heapq.heappop(arr)
+        new = a+b*2
+        heapq.heappush(arr, new)
+    print(arr)
+    if arr[0]<K:
+        return -1
+    return i
+
