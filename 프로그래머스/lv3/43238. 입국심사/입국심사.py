66142bfe3b061abb147a88383a1b3d181ece45d8
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2023-02-05 18:13:03 +0900

    [level 3] Title: 입국심사, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43238. 입국심사/입국심사.py
@@ -0,0 +1,16 @@
+def solution(n, times):
+    l,r = 1,max(times)*n
+    ans = 0
+    while l<=r:
+        m = (l+r)//2
+        x = 0
+        for t in times:
+            x+=m//t
+            if x>=n: break
+        if n>x:
+            l=m+1
+        else: 
+            ans = m
+            r=m-1
+    print(ans)
+    return ans
