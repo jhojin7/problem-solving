f87432394e401c19d01de31a268654c0cf6181fa
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2023-04-08 00:00:11 +0900

    [Unrated] Title: 당신은 운명을 믿나요?, Time: 144 ms, Memory: 127092 KB -BaekjoonHub

---
+++ 백준/Unrated/27930. 당신은 운명을 믿나요？/당신은 운명을 믿나요？.py
@@ -0,0 +1,15 @@
+y = set("YONSEI".strip())
+k = set("KOREA".strip())
+
+s = list(input())
+for c in s:
+    if c in y:
+        y.remove(c)
+    if c in k:
+        k.remove(c)
+    if not y:
+        print("YONSEI")
+        exit()
+    if not k:
+        print("KOREA")
+        exit()
