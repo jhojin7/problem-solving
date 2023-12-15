5816d1bad0404002092682627abcdf4543bdaa92
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2023-12-16 04:40:05 +0900

    [Gold V] Title: LCS, Time: 168 ms, Memory: 19164 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s1 = br.readLine();
+        String s2 = br.readLine();
+        int[][] dp = new int[1 + s1.length()][1 + s2.length()];
+        for (int i = 1; i < 1 + s1.length(); i++) {
+            for (int j = 1; j < 1 + s2.length(); j++) {
+                char a = s1.charAt(i - 1);
+                char b = s2.charAt(j - 1);
+                if (a == b) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        System.out.println(dp[s1.length()][s2.length()]);
+    }
+}
