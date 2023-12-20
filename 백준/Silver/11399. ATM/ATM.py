7f5e8cfb7aceb0592f01d99ed211c79ceb665e1c
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2023-12-20 11:27:32 +0900

    [Silver IV] Title: ATM, Time: 116 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,9 +1,4 @@
-N = int(input())
-people = list(map(int,input().split()))
-people.sort()
-# print(people)
-sum, rolling_sum = 0, 0
-for i in range(N):
-    sum += rolling_sum + people[i]
-    rolling_sum += people[i]
-print(sum)
+input()
+arr = sorted(map(int, input().split()))
+ans = [sum(arr[:i]) for i in range(len(arr)+1)]
+print(sum(ans))
