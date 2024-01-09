18a376ff3b90e4d038a5836af44f3c34884fd485
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2024-01-09 15:54:32 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 5.66 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/README.md
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/README.md
@@ -1,10 +1,10 @@
 # [level unrated] 가장 많이 받은 선물 - 258712 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/258712?language=python3) 
 
 ### 성능 요약
 
-메모리: 7.82 MB, 시간: 9.97 ms
+메모리: 10.5 MB, 시간: 5.66 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 15:53:26
+2024년 1월 2일 15:54:31
 
 ### 문제 설명
 
--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.py
@@ -4,14 +4,13 @@ def solution(friends, gifts):
     N = len(friends)
     cnt = [[0 for _ in range(N)] for _ in range(N)]
     for ab in gifts:
-    	a,b = ab.split()
+        a,b = ab.split()
         a,b = friends.index(a), friends.index(b)
         cnt[a][b] +=1
-    print(cnt)
-    
+        
     ans = [[0,0,0,0] for _ in range(N)]
     for ab in gifts:
-    	a,b = ab.split()
+        a,b = ab.split()
         a,b = friends.index(a), friends.index(b)
         ans[a][0] = sum(cnt[a])
     for a in range(N):
@@ -31,7 +30,4 @@ def solution(friends, gifts):
                     ans[a][3]+=1
                 elif ans[a][2] < ans[b][2]:
                     ans[b][3]+=1
-                    
-    print(ans)
     return max([ans[i][3] for i in range(N)])
-        
