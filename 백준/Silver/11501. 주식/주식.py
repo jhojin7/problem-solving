e57924444e2c5bb8dd80a3e1a6c1c5d4d3611263
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2024-03-27 20:15:18 +0900

    [Silver II] Title: 주식, Time: 1400 ms, Memory: 333956 KB -BaekjoonHub

--- 백준/Silver/11501. 주식/주식.py
+++ 백준/Silver/11501. 주식/주식.py
@@ -1,17 +1,11 @@
 for _ in range(int(input())):
-    N=int(input())
-    arr=list(map(int,input().split()))
-    profit=0
-    M = arr[-1]
+    N = int(input())
+    arr = list(map(int, input().split()))
+    ans =0
+    m = arr[-1]
     for i in range(N-1,-1,-1):
-        if arr[i]>M:
-            # update max
-            M=arr[i]
+        if m<arr[i]:
+            m = arr[i]
         else:
-            # add profit
-            profit+=M-arr[i]
-        # print(M,profit)
-    print(profit)
-
-# https://www.acmicpc.net/board/view/16527
-# 뒤에서부터-> 커지면 최고값 갱신, 작아지면 그 차이를 이득으로.
+            ans += m-arr[i]
+    print(ans)
