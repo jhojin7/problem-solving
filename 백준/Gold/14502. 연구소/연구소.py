d7d85c5fcb80ea4b79aed6e14ab28157c8b692fd
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2026-04-04 17:37:04 +0900

    [Gold IV] Title: 연구소, Time: 1188 ms, Memory: 118376 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,57 +1,50 @@
-import collections
+import itertools, collections, copy
 
-N,M = map(int,input().split())
-grid = [list(map(int,input().split())) for _ in range(N)]
-empty = []
-for i in range(N):
-    for j in range(M):
-        if grid[i][j]==0:
-            empty.append((i,j))
+N, M = map(int, input().split())
+_arr = [list(map(int, input().split())) for _ in range(N)]
+zeros = [(i, j) for i in range(N) for j in range(M) if _arr[i][j] == 0]
+perms = itertools.combinations(zeros, 3)
 
-dxy = [(-1,0),(0,-1),(1,0),(0,1)]
-def spread(_grid,xx,yy):
-    q=collections.deque([(xx,yy)])
+
+def bfs(xx, yy):
+    dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    q = collections.deque()
     vis = set()
+    q.append((xx, yy))
     while q:
-        x,y = q.popleft()
-        if (x,y) in vis: continue
-        vis.add((x,y))
-        _grid[x][y] = 2
-        for dx,dy in dxy:
-            nx,ny = x+dx,y+dy
-            if not (0<=nx<N and 0<=ny<M):
+        x, y = q.popleft()
+        if (x, y) in vis:
+            continue
+        vis.add((x, y))
+        arr[x][y] = 2
+        for dx, dy in dxy:
+            nx, ny = x + dx, y + dy
+            if not 0 <= nx < N:
                 continue
-            if _grid[nx][ny]==1: continue
-            if _grid[nx][ny]==2: continue
-            q.append((nx,ny))
+            if not 0 <= ny < M:
+                continue
+            if arr[nx][ny] == 0:
+                q.append((nx, ny))
 
-def check(_grid):
+
+def run():
     for i in range(N):
         for j in range(M):
-            if _grid[i][j]==2:
-                spread(_grid,i,j)
+            if arr[i][j] == 2:
+                bfs(i, j)
+    tmpcnt = 0
+    for row in arr:
+        tmpcnt += row.count(0)
+    return tmpcnt
 
-import copy, itertools
-combs = itertools.combinations(empty,3)
-ans =0
 
-for comb in combs:
-    _grid = copy.deepcopy(grid)
-    for i,j in comb:
-        _grid[i][j]=1
-    check(_grid)
-    tmp =0
-    for row in _grid:
-        tmp+=row.count(0)
-    ans = max(ans,tmp)
+ans = -(9**999)
+for perm in perms:
+    arr = copy.deepcopy(_arr)
+    for x, y in perm:
+        arr[x][y] = 1
+    cnt = run()
+    ans = max(ans, cnt)
+    for x, y in perm:
+        arr[x][y] = 0
 print(ans)
-
-# def backtrack(idx,wall):
-#     if wall==3:
-#         print()
-#         return
-#     for i in range(len(empty)):
-#         if i==idx: continue
-#         print(i,end=' ')
-#         backtrack(i,wall+1)
-# backtrack(0,0)
