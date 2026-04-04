bbb79cc7a8cb732cba38d6311b5b11265e2b7229
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2026-04-04 12:04:46 +0900

    [Silver IV] Title: 스택, Time: 408 ms, Memory: 112032 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,21 +1,23 @@
-import sys, collections
-input = sys.stdin.readline
-
 N = int(input())
-stack = collections.deque()
+stack =[]
 for _ in range(N):
-    query = input().split()
-    if query[0] == "push":
-        stack.append(query[1])
-    elif query[0] == "pop":
-        try: print(stack.pop())
-        except: print(-1)
-    elif query[0] == "size":
+    cmd, *args = input().split()
+    if cmd == "push":
+        stack.append(*args)
+    elif cmd == "pop":
+        if not stack: 
+            print(-1)
+            continue
+        x = stack.pop()
+        print(x)
+    elif cmd=="size":
         print(len(stack))
-    elif query[0] == "empty":
-        if len(stack) == 0: print(1)
-        else: print(0)
-    elif query[0] == "top":
-        if len(stack) != 0:
+    elif cmd=="empty" and not stack:
+        print(1)
+    elif cmd=="empty" and stack:
+        print(0)
+    elif cmd=="top":
+        if not stack: 
+            print(-1)
+        else:
             print(stack[-1])
-        else: print(-1)
