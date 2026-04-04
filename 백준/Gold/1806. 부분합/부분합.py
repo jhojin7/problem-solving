fdad6f53128854d54b2f070b89312311aa0a7938
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2026-04-04 13:18:25 +0900

    [Gold IV] Title: 부분합, Time: 128 ms, Memory: 121876 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,19 +1,28 @@
-N,S=map(int,input().split())
-arr = list(map(int,input().split()))
-pre = [0 for _ in range(N+1)]
-s=0
+N, S = map(int, input().split())
+arr = list(map(int, input().split()))
+presum = [0 for _ in range(N + 1)]
+# presum[0] = arr[0]
+# for i in range(1, N):
+#     presum[i] = presum[i - 1] + arr[i]
 for i in range(N):
-    s+=arr[i]
-    pre[i+1] = s
+    presum[i + 1] = presum[i] + arr[i]
+# presum[N] = presum[N - 1]
 
-l,r = 0,1
-ans=9999999
-while l<=r and r<N+1:
-    if pre[r]-pre[l]>=S:
-        # print(arr[l:r])
-        ans = min(ans,r-l)
-        l+=1
+l, r = 0, 0
+ans = 9**100
+
+flag = False
+while l <= r and r < len(arr) + 1:
+    ss = presum[r] - presum[l]
+    if ss >= S:
+        # print(l, r, ss)
+        l += 1
+        ans = min(ans, r - l + 1)
+        flag = True
     else:
-        r+=1
-if ans==9999999: print(0)
-else: print(ans)
+        r += 1
+
+if not flag:
+    print(0)
+else:
+    print(ans)
