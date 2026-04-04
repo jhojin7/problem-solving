d67f610c8748284e5f9c1d563ee7a2a3a565ffd2
Hojin Jang <23309485+jhojin7@users.noreply.github.com>
2026-04-04 16:28:36 +0900

    [Gold V] Title: 두 용액, Time: 124 ms, Memory: 124112 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -1,23 +1,17 @@
-import sys, collections, itertools, heapq, math, re, random
-input=sys.stdin.readline
-def inputint():return map(int,input().split())
-def inputchars():return input().strip()
-
-N=int(input())
-A = list(inputint())
-A.sort()
-
-l,r = 0,N-1
-ans = math.inf
-aa,bb = A[l],A[r]
-while l<r:
-    B = A[l]+A[r]
-    # print(A[l],A[r],B)
-    if abs(B)<abs(ans):
-        ans = B
-        aa,bb = A[l],A[r]
-    if B<0:
-        l+=1
+N = int(input())
+arr = sorted(map(int, input().split()))
+# print(arr)
+l, r = 0, len(arr) - 1
+ans = 9 ** 999
+ll, rr = -99, -99
+while l < r:
+    tmp = arr[l] + arr[r]
+    # print(l, r, arr[l] + arr[r])
+    if abs(tmp) <= abs(ans):
+        ans = tmp
+        ll, rr = l, r
+    if tmp < 0:
+        l += 1
     else:
-        r-=1
-print(min(aa,bb),max(aa,bb))
+        r -= 1
+print(arr[ll], arr[rr])
